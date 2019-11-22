a77e822e719a720ce32793b6d25f40fb0f36f6ef
yuqibuertong <2748736564@qq.com>
2019-11-22 14:55:23 +0800

    hw6

---
+++ hw8/Assets/Scripts/bloodBar_GUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bloodBar_GUI : MonoBehaviour {
+    private float curBlood = 0f;
+    private float targetBlood = 0f;
+    public Slider bloodBar;
+    GameObject btnPlus, btnMinus;
+
+    private void Start() {
+        btnPlus = GameObject.Find("addButton");
+        Button a = btnPlus.GetComponent<Button>();
+        btnMinus = GameObject.Find("minusButton");
+        Button b = btnMinus.GetComponent<Button>();
+        a.onClick.AddListener(delegate () {
+            this.OnClick(btnPlus);
+        });
+        b.onClick.AddListener(delegate () {
+            this.OnClick(btnMinus);
+        });
+    }
+
+    private void OnClick(GameObject sender) {
+        if (sender.name == "addButton") addBlood();
+        if (sender.name == "minusButton") minusBlood();
+    }
+
+    public void addBlood() {
+        targetBlood = targetBlood - 1f < 0f ? 0f : targetBlood - 1f;
+    }
+
+    public void minusBlood() {
+        targetBlood = targetBlood + 1f > 10f ? 10f : targetBlood + 1f;
+    }
+
+    void Update() {
+        curBlood = Mathf.Lerp(curBlood, targetBlood, 0.1f);
+        bloodBar.value = curBlood;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward);
+    }
+}
---
+++ hw8/Assets/Scripts/bloodBar_script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bloodBar_script : MonoBehaviour {
+    public float curBlood = 10f;
+    private float targetBlood = 10f;
+    private Rect bloodBarArea;
+    private Rect addButton;
+    private Rect minusButton;
+    private Rect num1;
+    private Rect num2;
+    private Rect num3;
+    private bool status;
+
+	void Start () {
+        bloodBarArea = new Rect(Screen.width - 220, 20, 200, 50);
+        addButton = new Rect(Screen.width - 220, 50, 40, 20);
+        minusButton = new Rect(Screen.width - 60, 50, 40, 20);
+        num1 = new Rect(Screen.width - 220, 80, 40, 20);
+        num2 = new Rect(Screen.width - 140, 80, 40, 20);
+        num3 = new Rect(Screen.width - 60, 80, 40, 20);
+	}
+
+    public void addBlood(float num) {
+        targetBlood = targetBlood + num > 10f? 10f : targetBlood + num;
+    }
+
+    public void minusBlood(float num) {
+        targetBlood = targetBlood - num < 0f? 0f : targetBlood - num;
+    }
+
+    private void OnGUI() {
+        if (GUI.Button(addButton, " + ")) status = true;
+        if (GUI.Button(minusButton, " - ")) status = false;
+        if (status) {
+            if (GUI.Button(num1, " 1 ")) addBlood(1);
+            if (GUI.Button(num2, " 2 ")) addBlood(2);
+            if (GUI.Button(num3, " 3 ")) addBlood(3);
+        }
+        else {
+            if (GUI.Button(num1, " 1 ")) minusBlood(1);
+            if (GUI.Button(num2, " 2 ")) minusBlood(2);
+            if (GUI.Button(num3, " 3 ")) minusBlood(3);
+        }
+        curBlood = Mathf.Lerp(curBlood, targetBlood, 0.1f);
+        GUI.HorizontalScrollbar(bloodBarArea, 0f, curBlood, 0f, 10f);
+    }
+}
